fea7fc151ab9b5038e50d26fada39dfcb6f9be31
Seeky <58006653+SeekyCt@users.noreply.github.com>
2024-01-06 16:24:47 +0000

    Extra library exports

--- src/pyelf2rel/__init__.py
+++ src/pyelf2rel/__init__.py
@@ -6,12 +6,22 @@ from pyelf2rel.error import (
     MissingSymbolError,
     UnsupportedRelocationError,
 )
+from pyelf2rel.lst import decode_lst, decode_lst_symbol, encode_lst, encode_lst_symbol
+from pyelf2rel.rel import RelSymbol
 
 __all__ = [
     "elf_to_lst",
     "elf_to_rel",
+
     "DuplicateSymbolError",
     "LSTFormatError",
     "MissingSymbolError",
     "UnsupportedRelocationError",
+
+    "decode_lst",
+    "decode_lst_symbol",
+    "encode_lst",
+    "encode_lst_symbol",
+
+    "RelSymbol"
 ]
