8938561b4978ffb2a27eafc3825b0f63b083529e
Seeky <58006653+SeekyCt@users.noreply.github.com>
2024-01-06 20:31:12 +0000

    makelst

--- src/pyelf2rel/__init__.py
+++ src/pyelf2rel/__init__.py
@@ -1,4 +1,3 @@
-from pyelf2rel.elf2lst import elf_to_lst
 from pyelf2rel.elf2rel import elf_to_rel
 from pyelf2rel.error import (
     DuplicateSymbolError,
@@ -10,7 +9,6 @@ from pyelf2rel.lst import decode_lst, decode_lst_symbol, encode_lst, encode_lst_
 from pyelf2rel.rel import RelSymbol
 
 __all__ = [
-    "elf_to_lst",
     "elf_to_rel",
     "DuplicateSymbolError",
     "LSTFormatError",
