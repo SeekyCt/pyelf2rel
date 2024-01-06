8c9353b6c5268128c1a90f26b75fb738c4591f67
Seeky <58006653+SeekyCt@users.noreply.github.com>
2024-01-06 15:27:33 +0000

    elf2rel -> ttydtools tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -25,25 +25,25 @@ def test_spm_practice_codes():
     assert dat == expected
 
 
-def test_spm_core_match_elf2rel():
+def test_spm_core_match_ttyd_tools():
     name = "spm-core-2fd38f5"
 
     with open(f"tests/resources/{name}.elf", "rb") as plf:
         dat = elf_to_rel(2, plf, f"tests/resources/{name}.lst", match_ttyd_tools=True)
 
-    with open(f"tests/resources/{name}_me2r.rel", "rb") as rel:
+    with open(f"tests/resources/{name}_ttydt.rel", "rb") as rel:
         expected = rel.read()
 
     assert dat == expected
 
 
-def test_spm_practice_codes_match_elf2rel():
+def test_spm_practice_codes_match_ttyd_tools():
     name = "spm-practice-codes-b94a94a"
 
     with open(f"tests/resources/{name}.elf", "rb") as plf:
         dat = elf_to_rel(0x1000, plf, f"tests/resources/{name}.lst", match_ttyd_tools=True)
 
-    with open(f"tests/resources/{name}_me2r.rel", "rb") as rel:
+    with open(f"tests/resources/{name}_ttydt.rel", "rb") as rel:
         expected = rel.read()
 
     assert dat == expected
